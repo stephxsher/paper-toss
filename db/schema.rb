1c17c3d12fbd5940447deb85f451c9e8553ff8da
smallfryy <holly.m.peck@gmail.com>
2016-04-09 15:45:22 -0400

    add column to users. discovered how to set rails timezone to the users browsers timezone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409192626) do
+ActiveRecord::Schema.define(version: 20160409194129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,9 +76,10 @@ ActiveRecord::Schema.define(version: 20160409192626) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "location"
     t.datetime "hire_date"
     t.string   "position"
+    t.integer  "karma"
+    t.integer  "location_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
