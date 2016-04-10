a4f664cf4df272ae1b7e662fa7b9867b1cb43494
smallfryy <holly.m.peck@gmail.com>
2016-04-10 15:32:21 -0400

    finished timezone set-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  around_action :set_time_zone, if: :current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_timezone
 
   protected
 
@@ -15,7 +15,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_time_zone(&block)
-    Time.use_zone(current_user.time_zone, &block)
+  def set_timezone
+    tz = current_user ? current_user.time_zone : nil
+    Time.zone = tz || ActiveSupport::TimeZone["London"]
   end
 end
